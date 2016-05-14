4eb4238ba4913473a2ec420a9f2fe1e99826ddb3
Paul Irish <paul.irish@gmail.com>
2016-05-13 18:26:30 -0700

    add sass example.

---
+++ example/sass/example.js
@@ -0,0 +1,65 @@
+.content-navigation {
+  border-color: #3bbfce;
+  color: #2ca2af; }
+  .content-navigation #nested {
+    background-color: black; }
+
+.border {
+  padding: 8px;
+  margin: 8px;
+  border-color: #3bbfce; }
+
+table.hl {
+  margin: 2em 0; }
+  table.hl td.ln {
+    text-align: right; }
+
+li {
+  font-family: serif;
+  font-weight: bold;
+  font-size: 1.2em; }
+
+#data {
+  float: left;
+  margin-left: 10px; }
+  #data th {
+    text-align: center;
+    font-weight: bold; }
+  #data td, #data th {
+    padding: 2px; }
+
+body {
+  margin: 0;
+  padding: 3em 6em;
+  font-family: tahoma, arial, sans-serif;
+  color: #000; }
+
+.big, .nested {
+  font-size: 14px; }
+
+.nested {
+  border-radius: 3px; }
+  .nested p {
+    background: whitesmoke; }
+    .nested p a {
+      color: red; }
+
+#navigation a {
+  font-weight: bold;
+  text-decoration: none !important; }
+
+h1 {
+  font-size: 2.5em; }
+
+h2 {
+  font-size: 1.7em; }
+
+h1:before, h2:before {
+  content: "::"; }
+
+code {
+  font-family: courier, monospace;
+  font-size: 80%;
+  color: #418A8A; }
+
+/*# sourceMappingURL=looks.css.map */
